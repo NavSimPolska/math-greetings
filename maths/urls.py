bf37da3833fcb4b03e8af7eb35b839e2d86985b2
NavSimPolska <jacek.pietraszkiewicz@navsim.pl>
2021-09-03 09:31:14 +0200

    Zastosowano Template i context. poprawiono wywolanie maths/

--- maths/urls.py
+++ maths/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import math, add, sub, mul, div
 
 urlpatterns = [
-   path('maths/', math),
+   path('', math),
    path('add/<int:a>/<int:b>', add),
    path('sub/<int:a>/<int:b>', sub),
    path('mul/<int:a>/<int:b>', mul),
--- maths/views.py
+++ maths/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def math(request):
    return HttpResponse("Tu będzie matma")
 
 def add(request, a, b):
-   return HttpResponse(a + b)
+   a,b = int(a), int(b)
+   wynik = a + b
+   t = loader.get_template("maths/main.html")
+   c = {"a": a, "b": b, "operacja": "+", "wynik" : wynik}
+   return HttpResponse(t.render(c))
 
 def sub(request, a, b):
-   return HttpResponse(a - b)
+   a,b = int(a), int(b)
+   wynik = a - b
+   t = loader.get_template("maths/main.html")
+   c = {"a": a, "b": b, "operacja": "-", "wynik" : wynik}
+   return HttpResponse(t.render(c))
 
 def mul(request, a, b):
-   return HttpResponse(a * b)
+   a,b = int(a), int(b)
+   wynik = a * b
+   t = loader.get_template("maths/main.html")
+   c = {"a": a, "b": b, "operacja": "*", "wynik" : wynik}
+   return HttpResponse(t.render(c))
 
 def div(request, a, b):
+   a, b = int(a), int(b)
    if b == 0:
-           return HttpResponse("Pamietaj cholero nigdy nie dziel przez zero!")
-   return HttpResponse(a / b)
+      wynik = " Pamietaj cholero nigdy nie dziel przez zero!"
+   else:
+      wynik = str(a / b)
+   t = loader.get_template("maths/main.html")
+   c = {"a": a, "b": b, "operacja": "/", "wynik" : wynik.upper()}
+   return HttpResponse(t.render(c))
